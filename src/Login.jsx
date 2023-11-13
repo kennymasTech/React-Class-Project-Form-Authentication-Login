895a64d5f31e176760b53b0e09add633b8db923a
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-13 14:31:16 +0100

    a

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,6 +6,16 @@ const Login = () => {
     const userRef = useRef()
     const errRef = useRef()
 
+    const [ username, setUsername ] = useState("")
+    const [ password, setPassword ] = useState("")
+    const [ errMsg, setErrMsg ] = useState("")
+    const [ success, setSuccess ] = useState(false)
+
+
+    useEffect(() => {
+        useRef.current.focus()
+    }, [])
+
 
   return (
     <div>
