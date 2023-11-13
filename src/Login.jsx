a0a8509775caec563179a8dff36ec7c1658d6681
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-13 16:01:46 +0100

    a

--- src/Login.jsx
+++ src/Login.jsx
@@ -39,7 +39,7 @@ const Login = () => {
     <>
     { success ? (
         <section>
-            <h1> Sign Up Successfully </h1>
+            <h1> Sign In Successfully </h1>
             <p>
             <a href="home"> Bact To Home</a>
             </p>
