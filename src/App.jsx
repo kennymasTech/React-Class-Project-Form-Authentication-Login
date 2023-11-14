e6412c70095732f645c76a0ed2f8295ad16b37bc
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-14 19:00:51 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,51 @@
-
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Layout from './components/Layout'
-import Login from './components/Login'
-import LinkPage from './components/LinkPage'
-import Unauthorized from './components/Unauthorized'
-import Home from './components/Home'
-import Editor from './components/Editor'
-import Admin from './components/Admin'
-import Lounge from './components/Lounge'
-import Missing from './components/Missing'
-import RequireAuth from './components/RequireAuth'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Login from "./components/Login";
+import LinkPage from "./components/LinkPage";
+import Unauthorized from "./components/Unauthorized";
+import Home from "./components/Home";
+import Editor from "./components/Editor";
+import Admin from "./components/Admin";
+import Lounge from "./components/Lounge";
+import Missing from "./components/Missing";
+import RequireAuth from "./components/RequireAuth";
 
 const ROLES = {
-  "User": 2001,
-  "Editor": 1984,
-  "Admin": 5158
-}
+  User: 2001,
+  Editor: 1984,
+  Admin: 5158,
+};
 
 const App = () => {
   return (
-        <Routes>
-            <Route path='/' element={<Layout/>}>
-                { /* public routes */ }
-                <Route path='login' element={<Login/>}/>
-                <Route path='linkpage' element={<LinkPage/>}/>
-                <Route path='unauthorized' element={<Unauthorized/>}/>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
 
-              {/* private route */}
-              <Route element={<RequireAuth allowedRoles={[ROLES.User, ROLES.Editor, ROLES.Admin]}/>}>
-                <Route path='/' element={<Home/>}/>
-              </Route>
-              <Route element={<RequireAuth allowedRoles={[ROLES.Editor]}/>}>
-                <Route path='editor' element={<Editor/>}/>
-              </Route>
-              <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}>
-                <Route path='admin' element={<Admin/>}/>
-              </Route>
-              <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]}/>}>
-                <Route path='lounge' element={<Lounge/>}/>
-              </Route>
+        {/* private route */}
+          <Route path="/" element={<Home />} />
+        
+        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]} />}>
+          <Route path="editor" element={<Editor />} />
+        </Route>
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin" element={<Admin />} />
+        </Route>
+        <Route
+          element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]} />}
+        >
+          <Route path="lounge" element={<Lounge />} />
+        </Route>
 
-              {/* catch all errors */}
-              <Route path='*' element={<Missing/>}/>
-            </Route>
-        </Routes>
-  )
-}
+        {/* catch all errors */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
+};
 
-export default App
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,119 +1,116 @@
-
-import { useState, useEffect, useRef } from 'react'
-import axios from '../api/axios'
-import useAuth from '../hooks/useAuth'
-import { useNavigate, Link, useLocation } from 'react-router-dom'
-
+import { useState, useEffect, useRef } from "react";
+import axios from "../api/axios";
+import useAuth from "../hooks/useAuth";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+const LOGIN_URL = "/auth";
 
 const Login = () => {
-    const LOGIN_URL = "/auth"
-    const { setAuth } = useAuth()
+  const { setAuth } = useAuth();
 
-    const navigate = useNavigate()
-    const location = useLocation()
-    const from = location.state?.from?.pathname || '/'
-    const userRef = useRef()
-    const errRef = useRef()
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const [ user, setUser ] = useState("")
-    const [ pwd, setPwd ] = useState("")
-    const [ errMsg, setErrMsg ] = useState("")
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
 
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
-    useEffect(() => {
-        userRef.current.focus()
-    }, [])
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
 
-    useEffect(() => {
-        setErrMsg('')
-    }, [user, pwd])
-
-    
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const response = await axios.post(LOGIN_URL, JSON.stringify({user, pwd}),{
-                headers: {'Content-Type': 'application/json'}, 
-                withCredentials: true 
-            })
-            console.log(JSON.stringify(response?.data));
-            const accessToken = response?.data?.accessToken
-            const roles = response?.data?.roles
-
-            setAuth({user, pwd, roles, accessToken})
-
-            setUser("");
-            setPwd("");
-            navigate(from, {replace: true})
-        } catch (error) {
-            if (!error.response) {
-                setErrMsg('No Server Response')
-            } else if (error.response?.status === 400) {
-                setErrMsg('Missing Username Or Password')
-            } else if (error.response?.status === 401) {
-                setErrMsg('Unauthorized')
-            } else {
-                setErrMsg('Login Failed')
-            }
-            errRef.current.focus();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        LOGIN_URL,
+        JSON.stringify({ user, pwd }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
         }
+      );
+      console.log(JSON.stringify(response?.data));
+      const accessToken = response?.data?.accessToken;
+      const roles = response?.data?.roles;
+
+      setAuth({ user, pwd, roles, accessToken });
+
+      setUser("");
+      setPwd("");
+      navigate(from, { replace: true });
+    } catch (error) {
+      if (!error.response) {
+        setErrMsg("No Server Response");
+      } else if (error.response?.status === 400) {
+        setErrMsg("Missing Username Or Password");
+      } else if (error.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
     //     if(!user || !pwd) {
     //         setErrMsg('Invalid Entry');
     //         return;
     // }
-        
-    }
-
+  };
 
   return (
-
     <section>
-        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
-            {errMsg}
-        </p>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
 
-        <h1> Sign In </h1>
+      <h1> Sign In </h1>
 
-        <form onSubmit={handleSubmit} >
-            <label htmlFor="user" >
-                User: 
-                <input 
-                    type="text" 
-                    id='username'
-                    ref={userRef}
-                    autoComplete='off'
-                    onChange={(e) => setUser(e.target.value)}
-                    value={user}
-                    required
-                 />
-            </label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="user">
+          User:
+          <input
+            type="text"
+            id="username"
+            ref={userRef}
+            autoComplete="off"
+            onChange={(e) => setUser(e.target.value)}
+            value={user}
+            required
+          />
+        </label>
 
-            <label htmlFor="password" >
-                Password: 
-                <input 
-                    type="password"
-                    id='password'
-                    onChange={(e) => setPwd(e.target.value)}
-                    value={pwd}
-                    required 
-                 />
-            </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPwd(e.target.value)}
+            value={pwd}
+            required
+          />
+        </label>
 
-            <button disabled={ !user || !pwd ? true : false }>
-                Sign In
-            </button>
+        <button disabled={!user || !pwd ? true : false}>Sign In</button>
+      </form>
 
-        </form>
-
-        <p>
-            Need An Account ? <br/>
-            <span>
-                <Link to="/register">Sign Up</Link>
-            </span>
-        </p>
+      <p>
+        Need An Account ? <br />
+        <span>
+          <Link to="/register">Sign Up</Link>
+        </span>
+      </p>
     </section>
-  )
-  
-}
+  );
+};
 
-export default Login
+export default Login;
