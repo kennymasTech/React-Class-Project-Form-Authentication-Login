92b3ecbc44a9fb701576370ecf7e7d0180099f3d
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-14 14:16:49 +0100

    a

---
+++ src/components/LinkPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const LinkPage = () => {
+  return (
+    <section>
+      <h1>Links</h1>
+      <br/>
+
+      <h2>Public</h2>
+      <Link to='login'>Login</Link>
+      {/* <Link to='/register'>Register</Link> */}
+    </section>
+  )
+}
+
+export default LinkPage
