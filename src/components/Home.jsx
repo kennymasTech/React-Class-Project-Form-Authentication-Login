de9c4dd3146ff84446666aed0ca1d376e689b7b4
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-14 14:07:34 +0100

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <section>
-        
+        <h1>Home</h1>
+        <br/>
+        <p>You're Logged In</p>
+        <br/>
+        <Link to='/editor'> Go To The Editor Page</Link>
     </section>
   )
 }
