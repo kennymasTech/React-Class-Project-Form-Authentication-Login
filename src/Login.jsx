62e60f073187dafefd4d1e5883d53c2d630ace22
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-14 08:58:50 +0100

    a

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,6 @@
 
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useContext } from 'react'
+import AuthContext from './context/AuthProvider'
 
 
 const Login = () => {
