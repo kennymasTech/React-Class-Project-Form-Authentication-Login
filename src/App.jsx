d19519ba0a784b149da24ac41b5bb115a4a2d7bb
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-14 14:37:41 +0100

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
 
 
 const App = () => {
   return (
     <main className='App' >
-
+        <Routes>
+          <Route path='/' element={<Layout/>}/>
+          
+        </Routes>
     </main>
   )
 }
