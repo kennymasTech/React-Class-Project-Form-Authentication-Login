f1fb9e37aae1166fcc721da948176113cdaa7155
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-14 14:33:36 +0100

    a

--- src/components/Unauthorized.jsx
+++ src/components/Unauthorized.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Unauthorized = () => {
+    const navigate = useNavigate()
+    const goBack = () => navigate()
   return (
     <section>
         <h1>Unauthorized ⚠️</h1>
         <br/>
         <p>You Do Not Have Access To The Requested Page.</p>
+
+        <div className="flexGrow">
+                <button>Go Back</button>
+        </div>
     </section>
   )
 }
