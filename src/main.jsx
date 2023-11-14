844472c0fe3630daa47aef4b6722e50543cf9eb5
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-14 12:35:55 +0100

    a

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './context/AuthProvider.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider >
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+            <Route path='/*' element={<App />}/>
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
