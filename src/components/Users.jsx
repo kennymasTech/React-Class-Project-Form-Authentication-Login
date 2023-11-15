40c2564cfe6e3a8537a1d56fb4ce28f65eb7ee68
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-15 10:47:29 +0100

    a

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { useState } from "react"
+
+
 
 const Users = () => {
+    const [ users, setUsers ] = useState()
   return (
-    <div>Users</div>
+    <article>
+        <h2>Users List</h2>
+        {users?.length 
+              ? (
+                    <ul>
+                        {users.map((user, i) => <li key={i}>{user?.username}</li>)}
+                    </ul>
+              ) : <p>No user to Display</p>
+            }
+    </article>
   )
 }
 
