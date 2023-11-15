ee6502797b2cfd898da398372bad7bbdb726d53b
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-15 14:44:27 +0100

    a

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -1,5 +1,9 @@
 
 import { axiosPrivate } from "../api/axios"
+import { useEffect } from "react"
+import useRefreshToken from "./useRefreshToken"
+import useAuth from "./useAuth"
+
 
 const useAxiosPrivate = () => {
   return (
