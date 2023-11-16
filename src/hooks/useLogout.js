ab9c5f0147b7bf04d912fec21bf16f5704957e1f
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-16 12:40:41 +0100

    a

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,20 @@
+import useAuth from "./useAuth"
+
+
+const useLogout = () => {
+    const { setAuth } = useAuth()
+
+    const logOut = async () => {
+        setAuth({});
+        try {
+            const response = await axios('/logout', {
+                withCredentials: true;
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+  return  logOut
+}
+
+export default useLogout
