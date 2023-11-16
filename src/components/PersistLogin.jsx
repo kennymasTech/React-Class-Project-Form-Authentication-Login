2a4628b0a3a948f2e04755cc95c45bf6dc9f77d6
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-16 11:58:36 +0100

    n

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -29,6 +29,16 @@ const PersistLogin = () => {
                 console.log(`isLoading: ${isLoading}`);
                 console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
         }, [isLoading])
+
+        return (
+            <>
+                {isLoading ? 
+                        <p>Loading...</p>
+                        :
+                        <Outlet/>
+                }
+            </>
+        )
 }
 
 
