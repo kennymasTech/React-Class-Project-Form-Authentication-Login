af628dac603ea48eeac038ef7bdcb45cf6e37ab7
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-16 12:43:40 +0100

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,7 @@
 import { useNavigate, Link } from "react-router-dom";
-import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
+
 
 const Home = () => {
-    const { setAuth } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const logout = async () => {
