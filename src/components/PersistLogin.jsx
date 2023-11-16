2fb4884f0ecb060971dadac815abaf0bceb22e5b
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-16 11:43:31 +0100

    a

--- src/components/PersistLogin.jsx
+++ src/components/PersistLogin.jsx
@@ -2,3 +2,13 @@ import { Outlet } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useRefreshToken from "../hooks/useRefreshToken";
 import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () => {
+        const [ isLoading, setIsLoading ] = useState()
+        const refresh = useRefreshToken()
+
+        const { auth } = useAuth()
+}
+
+
+export default PersistLogin
