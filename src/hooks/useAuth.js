9e4db7f169ba7f5b8724be76dd2e7adf0f2a4d31
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-17 20:20:28 +0100

    m

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -6,6 +6,6 @@ const useAuth = () => {
   const {auth} = useContext(AuthContext)
   useDebugValue(auth, auth => auth?.user? "Logged In" : "Logged Out")
   return useContext(AuthContext)
-}
+};
 
 export default useAuth
