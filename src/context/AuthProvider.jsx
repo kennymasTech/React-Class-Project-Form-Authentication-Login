930d24488ff61f4dccd75da7e9d552a5a80d66b2
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-17 20:05:55 +0100

    a

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,7 +1,7 @@
 
 import {createContext, useContext, useState } from "react";
 
-const AuthContext = createContext({})
+const AuthContext = createContext({});
 
 export const AuthProvider = ({children}) => {
     const [auth, setAuth] = useState({})
