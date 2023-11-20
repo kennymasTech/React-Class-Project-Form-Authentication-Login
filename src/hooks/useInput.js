6997327a16dea184e8d43cd13cdf579cdee576e8
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-20 10:35:27 +0100

    a

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -4,8 +4,13 @@ import { useState } from "react"
 
 
 const useInput = (initValue) => {
- const [ value, setValue ] = useState(initValue)
- 
+    const [ value, setValue ] = useState(initValue)
+    const reset = () => setValue(initValue)
+
+    const attributeObj = {
+        value,
+        onChange: (e) => setValue(e.target.value)
+    }
 }
 
 export default useInput
