4c7606c4744c4523cb11b48e3d8f51c10b3614ee
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-20 09:40:58 +0100

    a

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react"
 
 const useLocalStorage = () => {
-    const [ value, getValue ] = useState(JSON.parse(localStorage.getItem(key)) || 
+    const [ value, setValue ] = useState(JSON.parse(localStorage.getItem(key)) || 
     initValue)
 
     useEffect (() => {
         localStorage.setItem(key, JSON.stringify(value))
     }, [key, value])
  
-    return [key, setValue]
+    return [value, setValue]
 }
 
 export default useLocalStorage
